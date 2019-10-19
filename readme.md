a7a5e24d8e51a3625b2823901ee833adb9f27214
yoga <kaelan.ms@gmail.com>
2019-10-19 12:48:55 -0400

    added empty unit test framework and basic boilerplate http req

--- readme.md
+++ readme.md
@@ -4,3 +4,9 @@ https://www.pyimagesearch.com/2018/08/20/opencv-text-detection-east-text-detecto
 TODO: use these instructions to set up a venv
 https://stackoverflow.com/questions/6590688/is-it-bad-to-have-my-virtualenv-directory-inside-my-git-repository
 
+
+
+
+notes:
+flask blueprints are nice - a way to seperate your umm routes into different files
+according to our boi, none of the routes should be in the app
--- server.py
+++ server.py
@@ -1,12 +1,21 @@
 import opencv_test_funcs
 from flask import Flask
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
 
 @app.route('/')
 def default():
     return 'test'
 
-@app.route('/request')
-def request():
-    return 'Hello, World!'
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/uploadImg', methods=["POST"])
+def uploadImg():
+    logger.log(request.files)
+    return 'test'
+
 
