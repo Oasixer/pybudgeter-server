a1d8a38b7a212910bf457385b6e377519aee3f46
prairir <me@ryanprairie.com>
2019-10-19 17:03:05 -0400

    made a good file structure

--- app/controllers.py
+++ app/controllers.py
@@ -1,12 +1,7 @@
 import opencv_test_funcs
-from flask import Flask
 import sqlite3
 
-conn = sqlite3.connect('database.db')
-
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
-app = Flask(__name__)
 
 @app.route('/')
 def default():
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+DEBUG = True
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+THREADS_PER_PAGE = 2
+
