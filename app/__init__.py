bd3374a1487084b9573ad4d649c60bcf09b4da14
Kaelan M-S <kaelan.ms@gmail.com>
2019-12-07 16:02:03 -0500

    big ol juicy refactor

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,49 @@
-from flask import Flask
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask import Flask, request, current_app
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
 
-app = Flask(__name__)
+db = SQLAlchemy()
+#  login = LoginManager()
+#  login.login_view = 'auth.login'
+#  login.login_message = _l('Please log in to access this page.')
 
-app.config.from_object('config')
 
-db = SQLAlchemy(app)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-#from app.mod_auth.controllers import mod_auth as auth_module
-from app.controllers import things
+    db.init_app(app)
+    #  login.init_app(app)
 
-app.register_blueprint(things, url_prefix='/things')
+    #  from app.errors import bp as errors_bp
+    #  app.register_blueprint(errors_bp)
 
-#db.create_all()
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    if not app.debug and not app.testing:
+        # production
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+
+        #  file_handler = RotatingFileHandler('logs/microblog.log',
+                                           #  maxBytes=10240, backupCount=10)
+        #  file_handler.setFormatter(logging.Formatter(
+            #  '%(asctime)s %(levelname)s: %(message)s '
+            #  '[in %(pathname)s:%(lineno)d]'))
+        #  file_handler.setLevel(logging.INFO)
+        #  app.logger.addHandler(file_handler)
+
+        #  app.logger.setLevel(logging.INFO)
+        #  app.logger.info('Microblog startup')
+
+    return app
+
+from app import models
